39aadac518b2b4fa5d1c1f40f8d9053d0927cd0d
Parthan-Sachin <parthan.parth123@gmail.com>
2019-05-25 00:46:46 +0530

    Basic nav bar created and Home html basic desgin

--- Batallion/src/app/app.module.ts
+++ Batallion/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Batallion/src/app/product/products.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector:'app-prod',
+template:`
+<div>
+    <h1> product page</h1>
+</div>
+`
+
+})
+
+export class ProductComponent {
+
+    
+}
