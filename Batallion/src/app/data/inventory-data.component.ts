6ecfac1b5543a8263a9b817305e444e779bb540c
Parthan <parthan@techcreations.in>
2019-05-30 15:58:20 +0530

    added more data for testing

--- Batallion/src/app/data/inventory-data.component.ts
+++ Batallion/src/app/data/inventory-data.component.ts
@@ -7,12 +7,33 @@ import { Component } from '@angular/core';
 })
 
 export class InventoryDataComponent{
-    product = {
+    product = [{
         id:1,
         name:'wild-bohr',
         price:1200,
         instock: 'yes',
         image:'image'
+    },
+    {
+        id:2,
+        name: 'shark',
+        price:1500,
+        instock: 'yes',
+        image: 'image'
+    },
+    {
+        id:3,
+        name: 'mini punjab',
+        price: 3200,
+        instock: 'no',
+        image: 'nil'
+    },
+    {
+        id:4,
+        name: 'short bottle',
+        price: 2000,
+        instock: 'yes',
+        image: 'nil'
     }
-
+    ]
 }
