6d0f23821e7bda4b6724cf8aff927e50e8c757cd
Parthan <parthan@techcreations.in>
2019-05-30 18:38:17 +0530

    new nav bar

--- Batallion/src/app/shared/data.service.ts
+++ Batallion/src/app/shared/data.service.ts
@@ -10,27 +10,47 @@ const DATA = [
         name:'wild-bohr',
         price:1200,
         instock: 'yes',
-        image:'image'
+        image:'image',
+        category:'silencer'
     },
     {
         id:2,
         name: 'shark',
         price:1500,
         instock: 'yes',
-        image: 'image'
+        image: 'image',
+        category:'silencer'
     },
     {
         id:3,
         name: 'mini punjab',
         price: 3200,
         instock: 'no',
-        image: 'nil'
+        image: 'nil',
+        category:'silencer'
     },
     {
         id:4,
         name: 'short bottle',
         price: 2000,
         instock: 'yes',
-        image: 'nil'
+        image: 'nil',
+        category:'silencer'
+    },
+    {
+        id:5,
+        name: 'Jacket',
+        price: 7000,
+        instock: 'yes',
+        image:'nil',
+        category:'ridingGears'
+    },
+    {
+        id:6,
+        name: 'windshield',
+        price: 750,
+        instock:'yes',
+        image:'nil',
+        category: 'accessories'
     }
 ]
